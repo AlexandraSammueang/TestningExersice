fe6ad90b1e6306832294fd4fa27c1d2871d69f92
Alexandra Salén Sammueang <alexandra.salensammueang@edu.newton.se>
2021-11-26 14:57:14 +0100

    Add project files.

---
+++ Lektion2Exersice/Kronor.cs
@@ -0,0 +1,122 @@
+﻿using System;
+namespace Lektion2Exersice
+{
+    public class Kronor
+    {
+        public int ÖrenPart1;
+        /*
+         * Totala värdet i öre. 
+         * När vi väl har skapat ett Kronor-objekt ska det aldrig kunna ändras
+         */
+        private readonly int öre;
+        public int Öre => öre;
+
+        // Skapar tom Kronor
+        public Kronor()
+        {
+
+        }
+
+        // Skapar kopia av en annan Kronor
+        public Kronor(Kronor kronor)
+        {
+            öre = kronor.ÖrenPart() + kronor.KronorPart() * 100;
+        }
+        /*
+         * Skapar Kronor med initialt värde
+         */
+        public Kronor(int kronor, int öre)
+        {
+            this.öre = kronor * 100 + öre; // ändrar i constructor 
+        }
+
+        /*
+         * Returnerar kronordelen av kronorna
+         */
+        public int KronorPart()
+        {
+            return öre / 100;
+        }
+
+
+        /*
+         * Returnerar öresdelen av kronorna
+         */
+        public int ÖrenPart()
+        {
+            return öre % 100;
+        }
+        /*
+         * Adderar den här Kronor med other och returnerar resultatet
+         */
+
+        public Kronor Add(Kronor other)
+        {
+            if (other.isNegative())
+            {
+                throw new ArgumentException("Adding negative value");
+            }
+            var tempÖrenAddition = this.ÖrenPart() + other.ÖrenPart(); //tagit bort onödiga paranteser
+            var tempKronorAddition = this.KronorPart() * 100 + other.KronorPart() * 100; //ändrat här
+            if (tempKronorAddition < 0)
+            {
+                throw new ArgumentException("Negative value");
+            }
+
+            var tempKronor = tempKronorAddition / 100;
+
+            var tempÖren = tempÖrenAddition % 100;
+
+            return new Kronor(tempKronor, tempÖren);
+        }
+
+        public Kronor Subtract(Kronor other)
+        {
+
+            var tempÖrenSubbtration = this.ÖrenPart() - other.ÖrenPart();
+            var tempKronorSubtraction = this.KronorPart() * 100 - other.KronorPart() * 100;
+
+            var tempKronor = tempKronorSubtraction / 100;
+            var tempÖren = tempÖrenSubbtration % 100;
+
+            return new Kronor(tempKronor, tempÖren);
+
+            /* if (amount - other < 0)
+             {
+                 throw new ArgumentException("Negative amount");
+             }
+             return amount -= other;*/
+
+            //var temp = this.ÖrenPart() - other.KronorPart() + //tagit bort parentsen
+            //    (this.KronorPart() * 100 - other.ÖrenPart() * 10);
+            //var tempKronor = temp / 100;
+            //var tempÖren = temp % 100;
+
+            //return new Kronor(tempKronor, tempÖren);
+        }
+        /*
+         * Returnerar sant om kronorna har ett positivt värde
+         */
+        public bool isPositive()
+        {
+            return öre > 0;
+        }
+
+        /*
+         * Returnerar sant om kronorna har ett negativt värde
+         */
+        public bool isNegative()
+        {
+            return öre < 0;
+        }
+
+        /*
+         * Returnerar sant om kronorna har ett värde på 0
+         */
+        public bool isZero()
+        {
+            return öre == 0;
+        }
+    }
+}
+
---
+++ TestProject1/AccountTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Xunit;
+using Lektion2Exersice;
+
+namespace Lektion2.UnitTests
+{
+    public class AccountTests
+    {
+        [Fact]
+        public void Account_ChecksIfAmountIsEmpty()
+        {
+            var account = new Account();
+            Assert.True(account.Amount.isZero());
+
+        }
+
+        [Fact]
+        public void Deposit_CheckIfMoneyIsAdded()
+        {
+            var account = new Account();
+            account.Amount = new Kronor(5, 0);
+            var kronor = new Kronor(2, 0);
+
+            account.Deposit(kronor);
+            var actualValue1 = account.Amount.KronorPart();
+            //var actualValue2 = account.Amount.ÖrenPart();
+
+            Assert.Equal(7, actualValue1);
+            //Assert.Equal(2, actualValue2);
+
+        }
+
+        [Fact]
+        public void Withdraw_WhenAccountIsPosetive_OrInAccountMore90Persont()
+        {
+            var account = new Account();
+            account.Amount = new Kronor(9, 0);
+            var kronor = new Kronor(10, 0);
+
+            account.Withdraw(kronor);
+            var actualValue1 = account.Amount.KronorPart();
+            //var actualValue2 = account.Amount.ÖrenPart();
+
+            Assert.Equal(-1, actualValue1);
+            //Assert.Equal(2, actualValue2);
+
+        }
+
+        [Fact]
+        public void Withdraw_WhenMoneyLessThan90_ReturnExeption()
+        {
+            //Arrange
+            var account = new Account();
+            account.Amount = new Kronor(5, 0);
+            var kronor = new Kronor(7, 0);
+
+            //Act
+            Action act = () => account.Withdraw(kronor);
+
+            //Assert
+            Assert.Throws<ArgumentException>(act);
+
+        }
+
+        [Fact]
+        public void RemoveAll_WhenCalled_RemovesAllTheMoneyFrom_Account()
+        {
+            //Arrange
+            var account = new Account(new Kronor(10, 0));
+
+            //Act
+            account.RemoveAll();
+            var actualValue = account.Amount.KronorPart();
+
+            //Assert
+            Assert.Equal(0, actualValue);
+
+        }
+
+        [Fact]
+        public void Transfer_money_from_to()
+        {
+           
+            {
+                var kronor = new Kronor(11, 11);
+                var account1 = new Account(kronor);
+                var account2 = new Account();
+                Account.Transfer(account1, account2, kronor);
+                Assert.True(account1.Amount.isZero());
+                Assert.True(account2.Amount.isPositive());
+
+               
+
+            }
+
+        }
+
+    }
+}
